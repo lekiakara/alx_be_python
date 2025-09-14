764b1b1b5fee6cee7c4f62315dafe0d034681d08
Lekia Serekara <lekiakara@gmail.com>
2025-09-14 22:52:04 +0100

    Basic operations in python

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,10 @@
+# Define variables
+
 number1 = 10
 number2 = 5
+
+# Outputs
+
 Print('Addition of 10 and 5 is, number1 + number2)
 Print('Subtraction of 10 and 5 is, int(number1 - number2))
 Print('Multiplication of 10 and 5 is, int(number1 * number2))
