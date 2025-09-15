3fc95abff7f93e9ccefa09ac8865fc175d4203f0
Lekia Serekara <lekiakara@gmail.com>
2025-09-15 01:32:46 +0100

    Errors fixed in basic operations python exercise

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -6,12 +6,12 @@ number2 = 5
 # Calculate the sum, difference and product of number1 and number2
 
 calculate_the_sum = number1 + number2
-Print('Addition of 10 and 5 is:', number1 + number2)
+Print('Addition of 10 and 5 is:', calculate_the_sum)
 
 calculate_the_difference = number1 - number2
-Print('Subtraction of 10 and 5 is:', number1 - number2)
+Print('Subtraction of 10 and 5 is:', calculate_the_difference)
 
 calculate_the_product = number1 * number2
-Print('Multiplication of 10 and 5 is:', int(number1 * number2)
+Print('Multiplication of 10 and 5 is:', calculate_the_product)
 
 
