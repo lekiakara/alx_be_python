52215f2b2ebe40a650c1bf3880dc01170dd70f4a
Lekia Serekara <lekiakara@gmail.com>
2025-09-15 02:04:52 +0100

    Fixed errors in finances calculator exercise in python

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,8 @@
 # Define user input variables
 
+monthly_income = 5000
+total_monthly_expenses = 4000
+
 monthly_income = int(input("Enter your monthly income")
 total_monthly_expenses = int(input("Enter your total monthly expenses")
 
@@ -11,5 +14,5 @@ print("Your monthly savings are $:", your_monthly_savings)
 # Projected annual savings
 
 simple_annual_interest = 0.05
-projected_savings = monthly_savings * 12 + (monthly_ssavings * 12 * 0.05)
+projected_savings = 1000 * 12 + (1000 * 12 * 0.05)
 print("Projected savings after one year, with interest, is:", projected_savings))
