4c50a1210ab36bb3fa0271c23a07053256975876
Lekia Serekara <lekiakara@gmail.com>
2025-09-19 11:25:00 +0100

    Add exercise in python: weather advice

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,16 @@
+# Prompt user for weather input
+
+weather = [sunny, rainy, cold]
+
+user = input("What's the weather like today? (sunny, rainy, cold))
+if weather is sunny: 
+print("Wear a t-shirt and sunglasses")
+
+elif weather is rainy:
+print("Don't forget your umbrella and raincoat.")
+
+elif weather is cold:
+print("Make sure to wear a warm coat and a scarf.")
+
+else:
+print("Sorry, I don't have recommendations for you"
--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -10,14 +10,15 @@ total_monthly_expenses = int(input("Enter your total monthly expenses:"))
 
 # Calculate monthly savings
 
-monthly_savings = monthly_income - monthly_expenses
+monthly_savings = monthly_income - total_monthly_expenses
 print("Your monthly savings are $:", monthly_savings)
 
 # Projected annual savings
 
 simple_interest = 0.05
-projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05))
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 
 print("Projected savings after one year, with interest, is:", projected_savings)
+
 
