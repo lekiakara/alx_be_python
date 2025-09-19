5e741abe3d213f0d8462bed7c9acba0c13dbec97
Lekia Serekara <lekiakara@gmail.com>
2025-09-19 13:26:32 +0100

    Add exercise in python: match case calculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,22 @@
+# Prompt user to enter numbers
+
+num1 = input("Enter the first number: ")
+num2 = input("Enter the Second number: ")
+operation = input("Choose the operation? (+, -, *, /):")
+
+match operation:
+    case "+":
+        result = int(num1) + int(num2) 
+        print("The result is:", result)
+    
+    case "-":
+        result = int(num1) - int(num2)
+        print("The result is:", result)
+        
+    case "*":
+        result = int(num1) * int(num2)
+        print("The result is:", result)
+    
+    case "/":
+        result = int(num1) / int(num2)
+        print("The result is:", result)
