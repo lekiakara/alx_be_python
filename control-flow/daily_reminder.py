4008f07b0918c213031c7ceb5d887d4071e6c72b
Lekia Serekara <lekiakara@gmail.com>
2025-09-21 23:37:10 +0100

    update

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -8,19 +8,17 @@ priority = input("Priority (high/medium/low): ")
 # Process using match case for priority
 match priority:
     case "high":
-        reminder = f"Reminder: '{task}' is a high priority task. Give it maximum attention."
+        reminder = f"Reminder: '{task}' is a high priority task."
     case "medium":
-        reminder = f"Reminder: '{task}' is a medium priority task. Make sure you complete it within the specified period of time."
+        reminder = f"Reminder: '{task}' is a medium priority task."
     case "low":
-        reminder = f"Reminder: '{task}' is a low priority task. Consider completing it when you have free time."
+        reminder = f"Reminder: '{task}' is a low priority task."
     case _:
-        reminder = f"Reminder: '{task}' has an unknown priority level. Do not give it any attention."
+        reminder = f"Reminder: '{task}' has an unknown priority level."
 
 # Modify reminder if task is time-sensitive
 if time_bound == "yes":
-    reminder += "It requires immediate attention today!"
-
-    
+    reminder = "Finish project report' is a high priority task that requires immediate attention today!"  
 
 # Print final customized reminder
 print(reminder) 
