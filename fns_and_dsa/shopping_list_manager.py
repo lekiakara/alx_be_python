e21b106729a2d528d47520e02b6bf31962493386
Lekia Serekara <lekiakara@gmail.com>
2025-09-29 02:04:09 +0100

    upate

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,8 +1,8 @@
 def display_menu():
     print("Shopping List Manager")
-    print("1. Add an item")
-    print("2. Remove an item")
-    print("3. View the list")
+    print("1. Add item")
+    print("2. Remove item")
+    print("3. View list")
     print("4. Exit")
 
 def main():
@@ -11,16 +11,31 @@ def main():
     while True:
         display_menu()
         choice = input("Enter your choice: ")
+        
 
         if choice == "1":
-            pass
+            item = input("Enter the item to add: ")
+            shopping_list.append(item)
+            print(f" '{item}' has been added to shoppin list.")
+        
         elif choice == "2":
-            pass
+            item = input("Enter the item to add: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f" '{item}' has been added to shoppin list.")
+            else:
+                print(f" '{item}' was not found in your shopping list.")
+            
         elif choice == "3":
-            pass
+           if item in shopping_list:
+               print("Your shopping list:")
+               for i, item in enumerate(shopping_list, start=1):
+                   (f"{i}. {item}")
+                   
         elif choice == "4":
-            print("Goodbye!")
+            print("Exiting shopping list Manager. Goodbye!")
             break
+        
         else:
             print("Invalid choice. Please enter a number between 1 and 4.")
 
