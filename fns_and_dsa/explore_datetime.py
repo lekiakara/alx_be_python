4d1960f95074dab1f61ec74d1ad37f52ed364694
Lekia Serekara <lekiakara@gmail.com>
2025-09-29 04:37:24 +0100

    update

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,7 +2,7 @@ import datetime
 
 def display_current_datetime():
     current_date = datetime.datetime.now()
-    formatted_date = current_date.strftime("%Y-%m-%d : %H:%M:%S")
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print("Current date and time is: ", formatted_date)
 
 display_current_datetime()
