e1e76476074730291b2518acd6a2b91636176437
Lekia Serekara <lekiakara@gmail.com>
2025-09-29 03:53:43 +0100

    update

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -11,7 +11,7 @@ from datetime import datetime, timedelta
 def calculate_future_date(days: int):
     today = datetime.today()
     future_date = today + timedelta(days=days)
-    formatted_date = future_date.strftime("%Y-%m-%d")
+    formatted_date = future_date.strftime("%Y-%m-%d : %H:%M:%S")
 
     # Print the result
     print(f"Future date: {formatted_date}")
