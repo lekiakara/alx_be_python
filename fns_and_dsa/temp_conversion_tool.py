433355e64ce4f5bb4ed59811ef94677e0f209057
Lekia Serekara <lekiakara@gmail.com>
2025-10-01 14:07:23 +0100

    update

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,11 +5,11 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 # Implement Conversion Functions
 def convert_to_celsius(fahrenheit):
-    return FAHRENHEIT_TO_CELSIUS_FACTOR
+    return FAHRENHEIT_TO_CELSIUS_FACTOR ; 5/9
 
 
 def convert_to_fahrenheit(celsius):
-    return CELSIUS_TO_FAHRENHEIT_FACTOR
+    return CELSIUS_TO_FAHRENHEIT_FACTOR ; 9/5
 
 
 # Main Program 
