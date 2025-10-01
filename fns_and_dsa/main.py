7099ba24a3d623fbb8e580d8e4352f76f4275852
Lekia Serekara <lekiakara@gmail.com>
2025-10-01 13:20:49 +0100

    Add python exercise: perform operation

---
+++ fns_and_dsa/main.py
@@ -0,0 +1,10 @@
+from arithmetic_operations import perform_operation
+
+num1 = 5
+num2 = 6
+
+print(perform_operation(num1, num2, "add"))       
+print(perform_operation(num1, num2, "subtract"))  
+print(perform_operation(num1, num2, "multiply")) 
+print(perform_operation(num1, num2, "divide"))   
+print(perform_operation(num1, num2, "modulus"))   
