949469f3e4f65d194ac031c9e25ffd955660ec34
Lekia Serekara <lekiakara@gmail.com>
2025-10-12 23:50:12 +0100

    update

--- oop/library_system.py
+++ oop/library_system.py
@@ -23,7 +23,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"Print Book: {self.title} by {self.author} {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author} {self.page_count}"
 
 
 class Library:
