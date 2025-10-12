0de4b48bc32cb8a24ecd8836ac2fa3d0f7495060
Lekia Serekara <lekiakara@gmail.com>
2025-10-12 23:48:32 +0100

    update

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,7 +5,7 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 
 class EBook(Book):
